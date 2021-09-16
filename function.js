04af8e9ea92cec7978b77e8d357f28bc58054412
BDDesigner <abdulmajed.cse555@gmail.com>
2021-09-16 19:04:15 +0600

    Add Some code

--- function.js
+++ function.js
@@ -1,7 +1,26 @@
+//Normal function 
 function displayName(a, b) {
     return a * b;
 }
+let x = displayName(4, 3);
 
-let result = displayName(10, 20);
+
+//fahrenheit to celsius formula
+function fahrenheightToCel(fahrenheight) {
+    const f = (fahrenheight-32) * 5/9;
+    return f;
+}
+
+let result = fahrenheightToCel(100);
 console.log(result);
+
+//celsius to fahrenheit formula
+function CelToFahren(cel) {
+    return (cel * 9/5) + 32;
+}
+
+const value = CelToFahren(100)
+console.log(value);
+
+
 
