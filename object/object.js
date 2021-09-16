347af3228ca7e3883dd5cf990d4f21c8325cfb9e
BDDesigner <abdulmajed.cse555@gmail.com>
2021-09-16 19:41:59 +0600

    source code push

---
+++ object/object.js
@@ -0,0 +1,12 @@
+const car = {
+    name: 'BMW',
+    color: 'Black',
+    price: '50k',
+    start: function () {
+        console.log('The Car is Start.');
+    },
+    stop: function () {
+        console.log('The Car is Stop.');
+    }
+}
+
