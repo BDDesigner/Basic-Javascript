4f6e9e50c366cd7924a5f26aa81bfa730ddf70b4
BDDesigner <abdulmajed.cse555@gmail.com>
2021-09-18 21:16:22 +0600

    lesson-03 source code push

---
+++ event.js
@@ -0,0 +1,5 @@
+function showDate() {
+    const date = document.getElementById('demo').innerHTML = Date();
+    return date;
+    
+}
--- object/object.js
+++ object/object.js
@@ -1,17 +1,4 @@
-const car = {
-    name: 'BMW',
-    color: 'Black',
-    price: '50k',
-    start: function () {
-        return 'The Car is Start';
-    },
-    stop: function () {
-        const result = this.start();
-        return result + " 10 minute Later The car was Stop.";
-    }
+function sayHi() {
+    var name = 'java';
+    let age = 21;
 }
-
-console.log(car.name);
-console.log(car.start());
-console.log(car.stop());
-
